f45908c673b8ecdcd226d6f0243bc465f4b68341
MirToykin <miroslav.toykin@gmail.com>
2020-04-22 21:06:16 +0300

    added SelectInput for component prop of redux-form's field

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -6,6 +6,7 @@ import {createEvent, updateEvent} from "../eventActions";
 import {Field, reduxForm} from "redux-form";
 import TextInput from "../../../app/common/form/TextInput";
 import Textarea from "../../../app/common/form/Textarea";
+import SelectInput from "../../../app/common/form/SelectInput";
 
 const mapState = (state, ownProps) => {
   const eventId = ownProps.match.params.id;
@@ -30,6 +31,15 @@ const actions = {
 }
 
 const EventForm = ({createEvent, updateEvent, event, history}) => {
+
+  const category = [
+    {key: 'drinks', text: 'Drinks', value: 'drinks'},
+    {key: 'culture', text: 'Culture', value: 'culture'},
+    {key: 'film', text: 'Film', value: 'film'},
+    {key: 'food', text: 'Food', value: 'food'},
+    {key: 'music', text: 'Music', value: 'music'},
+    {key: 'travel', text: 'Travel', value: 'travel'},
+  ];
 
   const [formFields, setFormFields] = useState(event);
 
@@ -59,8 +69,15 @@ const EventForm = ({createEvent, updateEvent, event, history}) => {
           <Form>
             <Header sub color='teal' content='Event Details'/>
             <Field name='title' component={TextInput} placeholder='Give your event a name'/>
-            <Field name='category' component={TextInput} placeholder='What is your event about?'/>
-            <Field name='description' rows={3} component={Textarea} placeholder='Tell us about your event'/>
+            <Field name='category'
+                   component={SelectInput}
+                   placeholder='What is your event about?'
+                   options={category}
+            />
+            <Field name='description'
+                   rows={3} component={Textarea}
+                   placeholder='Tell us about your event'
+            />
             <Header sub color='teal' content='Event Location Details'/>
             <Field name='city' component={TextInput} placeholder='Event City'/>
             <Field name='venue' component={TextInput} placeholder='Event Venue'/>
